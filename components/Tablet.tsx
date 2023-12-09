dc581e2aa20df7c65a72c7ce2bfb33c4d077a495
HZKMaurice <zikaiho@gmail.com>
2023-12-09 20:57:57 +0800

    refactor: reduce reads from database

--- components/Tablet.tsx
+++ components/Tablet.tsx
@@ -1,26 +1,21 @@
-export interface TabletProps {
-  block: string;
-  row: number;
-  number: number;
-  status: string;
-}
+import {Tablet} from '@/app/data/dataTypes';
 
-function Tablet(props: TabletProps) {
-  const { number, status, block } = props;
+
+function Tablet(props: Tablet) {
 
   let statusColorClass = '';
 
-  switch (status) {
-    case 'ipt in progress':
+  switch (props.Status.toString()) {
+    case 'IPT':
       statusColorClass = 'bg-green-500'; // Green
       break;
-    case 'reserved':
+    case 'Reserved':
       statusColorClass = 'bg-yellow-500'; // Yellow
       break;
-    case 'occupied':
+    case 'Occupied':
       statusColorClass = 'bg-red-500'; // Red
       break;
-    case 'blocked':
+    case 'Blocked':
       statusColorClass = 'bg-purple-500'; // Purple
       break;
     default:
@@ -28,11 +23,11 @@ function Tablet(props: TabletProps) {
   }
 
   return (
-    <div className={Number(number) === 2 && block!=="D" ? 'ml-7 ' : Number(number) === 1 && block==="D" ? 'ml-7' : ' '}>
-      <div className={`${statusColorClass} ${status === 'Not Available' ? 'w-[43px] h-[51px] px-3 pt-[25px] m-1' : 'w-[43px] h-[51px] px-3 pt-[25px] pb-1 border border-zinc-700 bg-white rounded shadow flex-col justify-end items-center inline-flex m-1'}`}>
-      {status !== 'Not Available' && (
+    <div className={Number(props.Column_Number) === 2 && props.Block!=="D" ? 'ml-7 ' : Number(props.Column_Number) === 1 && props.Block==="D" ? 'ml-7' : ' '}>
+      <div className={` ${props.Status === 'Not Available' ? 'w-[43px] h-[51px] px-3 pt-[25px] m-1' : `${statusColorClass} w-[43px] h-[51px] px-3 pt-[25px] pb-1 border border-zinc-700 rounded shadow flex-col justify-end items-center inline-flex m-1`}`}>
+      {props.Status !== 'Not Available' && (
         <div>
-          {number.toString()}
+          {props.Column_Number}
         </div>
       )}
       </div>
